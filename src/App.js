5c70ae6297cea2b116a6ad7182c3e95b69bf7089
Almas Meirmanov <almas1521@gmail.com>
2020-02-18 14:02:21 -0800

    like-button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
-function App() {
+function App(props) {
+  const [count, setCount] = useState(0)
+  var name = "likes"
+  function updateCount(e) {
+    e.preventDefault()
+    setCount(count + 1)
+  }
+
+  if (count === 1) {
+    name = "like"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="box">
+      <button onClick={updateCount}>
+        {count}
+        {name}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
